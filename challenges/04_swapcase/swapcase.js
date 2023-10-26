cef8cc928addd858e14ac1537fa6bbf2adbe7664
nine96as <nine96@skiff.com>
2023-10-26 21:40:05 +0100

    Complete swapcase.js task

--- challenges/04_swapcase/swapcase.js
+++ challenges/04_swapcase/swapcase.js
@@ -1,9 +1,13 @@
-const swapcase = input => {
-
-}
+const swapcase = (input) => {
+  return [...input].map((e) =>
+    e === e.toLowerCase() ? e.toUpperCase() : e.toLowerCase()
+  ).join``;
+};
 
 // Do not delete the code below 🙏
 
 module.exports = {
   swapcase
-}
+};
+
+console.log(swapcase('JournAl'));
