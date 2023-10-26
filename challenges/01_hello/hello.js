7059d7f209da15e21fef2758b98c20db2cdb2446
nine96as <nine96@skiff.com>
2023-10-26 21:24:02 +0100

    Complete hello.js task

--- challenges/01_hello/hello.js
+++ challenges/01_hello/hello.js
@@ -1,6 +1,9 @@
-
+const hello = (name) => {
+  if (name === '') throw 'no empty strings allowed';
+  return name ? `Hello ${name}` : `Hello Stranger`;
+};
 // Do not delete the code below 🙏
 
 module.exports = {
   hello
-}
+};
