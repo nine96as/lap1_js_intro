b7d318427650ea36e04fb5a0ef7eac0d7bc69e3e
nine96as <nine96@skiff.com>
2023-10-26 22:30:30 +0100

    Complete max.js task

--- challenges/11_max/max.js
+++ challenges/11_max/max.js
@@ -1,9 +1,12 @@
 const max = (arr) => {
-
-}
+  if (arr.length) return arr.reduce((a, b) => (b > a ? b : a), 0);
+};
 
 // Do not delete the code below 🙏
 
 module.exports = {
   max
-}
+};
+
+console.log(max([1, 297, -73, 99, 10]));
+console.log(max([]));
