06a38e32e68fcfe2ae9b0e8a60f463c141dde285
nine96as <nine96@skiff.com>
2023-10-26 22:09:48 +0100

    Complete upcase.js task

--- challenges/06_upcase/upcase.js
+++ challenges/06_upcase/upcase.js
@@ -1,9 +1,13 @@
 const upcase = (arr) => {
-
-}
+  let arr2 = [];
+  arr.forEach((element, index) => {
+    arr2.push(element.toUpperCase());
+  });
+  return arr2;
+};
 
 // Do not delete the code below 🙏
 
 module.exports = {
   upcase
-}
+};
