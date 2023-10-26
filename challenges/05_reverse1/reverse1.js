d18d00e769d30d26e310eb3698dd23cbebfddfbc
nine96as <nine96@skiff.com>
2023-10-26 21:49:10 +0100

    Complete reverse1.js task

--- challenges/05_reverse1/reverse1.js
+++ challenges/05_reverse1/reverse1.js
@@ -1,9 +1,15 @@
 const reverse1 = (word) => {
-
-}
+  let reversed = '';
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversed += word[i];
+  }
+  return reversed;
+};
 
 // Do not delete the code below 🙏
 
 module.exports = {
   reverse1
-}
+};
+
+console.log(reverse1('world'));
