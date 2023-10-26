41ccd1d67b5d744107b10fa0fc1e23a8a9ab3895
nine96as <nine96@skiff.com>
2023-10-26 21:35:08 +0100

    Complete capitalise.js task

--- challenges/03_capitalise/capitalise.js
+++ challenges/03_capitalise/capitalise.js
@@ -1,9 +1,9 @@
 const capitalise = (input) => {
-
-}
+  return input.slice(0, 1).toUpperCase() + input.slice(1).toLowerCase();
+};
 
 // Do not delete the code below 🙏
 
 module.exports = {
   capitalise
-}
+};
