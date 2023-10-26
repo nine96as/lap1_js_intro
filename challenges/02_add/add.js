99d776c94d201d53774461f754f8a2f07588b5bb
nine96as <nine96@skiff.com>
2023-10-26 21:31:15 +0100

    Complete add.js task

--- challenges/02_add/add.js
+++ challenges/02_add/add.js
@@ -1,7 +1,12 @@
-
+const add = (a, b) => {
+  if (!a && !b) throw 'you need to call the functions with 2 arguments';
+  if (!a || b) return b + b;
+  if (a || !b) return a + a;
+  return a + b;
+};
 
 // Do not delete the code below 🙏
 
 module.exports = {
   add
-}
+};
