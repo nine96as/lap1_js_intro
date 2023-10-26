8823fe545178d471248f58cae9254b2208b369e0
nine96as <nine96@skiff.com>
2023-10-26 22:46:42 +0100

    Complete sort-names.js task

--- challenges/13_sort-names/sort-names.js
+++ challenges/13_sort-names/sort-names.js
@@ -1,9 +1,12 @@
 const sortNames = (arr) => {
-
-}
+  // case-insensitive sorting ig
+  return arr.sort(Intl.Collator().compare);
+};
 
 // Do not delete the code below 🙏
 
 module.exports = {
   sortNames
-}
+};
+
+console.log(sortNames(['Ree', 'romeo', 'emile', 'sarah']));
