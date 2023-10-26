693ad5cc52217f6ada947d1ae671b29f0b123c82
nine96as <nine96@skiff.com>
2023-10-26 22:23:07 +0100

    Complete reverse3.js task

--- challenges/10_reverse3/reverse3.js
+++ challenges/10_reverse3/reverse3.js
@@ -1,9 +1,11 @@
 const reverse3 = (str) => {
-
-}
+  return str.split``.reverse().join``;
+};
 
 // Do not delete the code below 🙏
 
 module.exports = {
   reverse3
-}
+};
+
+console.log(reverse3('hello'));
